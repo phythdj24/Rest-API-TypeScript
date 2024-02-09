10768c8561a1af9492fa7a3a12e92586d5b3adca
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-09 22:23:14 +0530

    Some Changes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@
 // if (age < 50) 
 //     age += 10;
     
-// function sum(a: number,b: number):number{
-//     return a+b
-// }
+function sum(a: number,b: number):number{
+    return a+b
+}
 
 
-// console.log(sum(5,10));
+console.log(sum(5,10));
 
 
 let Myname = true;
